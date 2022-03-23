1f2b304a30f67a37e5fbe604610b91cb3bf35989
Julien Ferez <Julienferez@icloud.com>
2022-03-23 16:37:50 +0100

    Route Slug en totalité OK

--- pages/platforms/[platforms].tsx
+++ pages/platforms/[platforms].tsx
@@ -4,26 +4,46 @@ import { getDatabase } from "../../src/utils/database";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const mongodb = await getDatabase();
-  const data = JSON.stringify(context.params.platforms);
+
   const games = await mongodb
     .db()
     .collection("games")
-    .find({ "platform.name": `${data}` })
+    .find({ "platform.name": `${context.params.platforms}` })
     .toArray();
 
-  console.log("slug", data);
-  console.log("sortie après la recherhce mongo", games);
+  const gamesString = JSON.stringify(games);
 
   return {
     props: {
-      games: games,
+      games: gamesString,
     },
   };
 };
 export default function Games({ games }) {
+  const gamesJson = JSON.parse(games);
   return (
     <Layout>
-      <div>toto</div>
+      <div>
+        <div>
+          {gamesJson.map((game) => {
+            return (
+              <div className="container" style={{ maxWidth: "18rem" }}>
+                <div className="item">
+                  {game?.cover?.url ? (
+                    <img src={game.cover.url} style={{ maxHeight: "18rem" }} />
+                  ) : (
+                    <img src="..." style={{ maxHeight: "18rem" }} />
+                  )}
+                  <div>
+                    <h5>{game.name}</h5>
+                    <button>Ajouter au panier</button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Layout>
   );
 }
